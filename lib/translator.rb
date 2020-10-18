2a2e8c7b6af3b6ccf5252585fb023c2ee138dfff
Alexander Bowers <7319abowers@gmail.com>
2020-10-18 21:51:40 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,27 +7,37 @@ def load_library(filepath)
 
   language_hash = {}
   emoticons_hash.each do |emoticon, value|
-   language_hash[emoticon] = emoticons_hash[emoticon]
-   #binding.pry
- end
-
- language_hash.map do |emoticon, language|
-   language_hash[emoticon] = {:english => "", :japanese => ""}
-   language_hash[emoticon][:english] = emoticons_hash[emoticon][0]
-   language_hash[emoticon][:japanese] = emoticons_hash[emoticon][1]
- end
-  binding.pry
-  puts "hello"
+    language_hash[emoticon] = emoticons_hash[emoticon]
+    language_hash[emoticon] = {:english => "", :japanese => ""}
+    language_hash[emoticon][:english] = emoticons_hash[emoticon][0]
+    language_hash[emoticon][:japanese] = emoticons_hash[emoticon][1]
+  end
+  emoticons_hash = language_hash
+  emoticons_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
-  binding.pry
-  puts "hello"
-end
+def get_japanese_emoticon(filepath, emoticon)
+  emoticons_hash = load_library(filepath)
+  reply = "Sorry, that emoticon was not found"
 
-def get_english_meaning
-  # code goes here
-  binding.pry
-  puts "hello"
+    emoticons_hash.each do |key, emote|
+       if emoticons_hash[key][:english] == emoticon
+        reply = emoticons_hash[key][:japanese]
+      end
+    end
+    return reply
+  end
+#find emoticon in language_hash
+#my emoticons_hash
+
+def get_english_meaning(filepath, emoticon)
+emoticons_hash = load_library(filepath)
+reply = "Sorry, that emoticon was not found"
+
+  emoticons_hash.each do |key, emote|
+     if emoticons_hash[key][:japanese] == emoticon
+      reply = key
+    end
+  end
+  return reply
 end
